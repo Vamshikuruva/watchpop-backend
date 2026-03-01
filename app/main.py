93da9bd3576acc15cbfb5a5a72b00d76e65b0c4a
Vamshikuruva <buntyvamshi55@gmail.com>
2026-03-01 19:42:54 +0530

    add vercel link

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="Movies & Songs Watchlist")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://watchpop-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
